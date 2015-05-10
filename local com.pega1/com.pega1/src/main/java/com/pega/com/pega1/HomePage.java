0687678e4264b36d9e2d17528dc7deec97b46c00
vishnu <vishnuvenugopal73@gmail.com>
2015-05-10 21:05:10 +0530

    initial changes to the com.pega

---
+++ local com.pega1/com.pega1/src/main/java/com/pega/com/pega1/HomePage.java
@@ -0,0 +1,11 @@
+package com.pega.com.pega1;
+
+public class HomePage {
+
+	public void login(String string, String string2) {
+		// TODO Auto-generated method stub
+		System.out.println("hgjhgjgj");
+		
+	}
+
+}
---
+++ local com.pega1/com.pega1/src/test/java/com/pega/com/pega1/myMavenSelenium1.java
@@ -0,0 +1,24 @@
+package com.pega.com.pega1;
+
+import org.apache.log4j.Logger;
+//import org.apache.log4j.PropertyConfigurator;
+import org.testng.annotations.Test;
+
+public class myMavenSelenium1 {
+	
+//	PropertyConfigurator.
+	Logger logger = Logger.getLogger(myMavenSelenium1.class);
+
+	
+	@Test
+	public void startTest()
+	{
+		
+		System.out.println("ytetsuau");
+		HomePage homepage = new HomePage();
+		homepage.login("admin","password");
+		//logger.debug("test");
+		//logger.error("error");
+			}
+
+}
